7837239c6bbc7816f4fe509a4451d7d414352e71
albifhrzq <albiaja027@gmail.com>
2025-02-17 18:33:46 +0700

    Tugas-13(Laravel templating)

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selamat Datang</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('title')
+    halaman utama
+@endsection
+
+@section('content')
     <h1>SELAMAT DATANG!</h1>
     <p>Terima kasih sudah bergabung di Sanbercode.</p>
 
     @if(isset($data))
         <h2>Halo, {{ $data['first_name'] }} {{ $data['last_name'] }}!</h2>
     @endif
-</body>
-</html>
+@endsection
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -4,12 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
 
-// Route untuk halaman Home
 Route::get('/', [DashboardController::class, 'home'])->name('home');
 
-// Route untuk halaman Register
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'store'])->name('register.store');
 
-// Route untuk halaman Welcome
 Route::get('/welcome', [AuthController::class, 'welcome'])->name('welcome');
+
+Route::get('/data-table', function () {
+    return view('data-table');
+});
+
+Route::get('/table', function () {
+    return view('table');
+});
